10a2894e57f55f478646a5989f53ea3214be2456
Tommy Gabrielsen <>
2018-09-03 13:50:22 +0200

    Oppgave 1 og Oppgave 1 test, prelim

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,4 +1,19 @@
 public class Oblig1 {
 
+    public static int maks(int[] a) {
+        if (a.length < 1) {
+            throw new IllegalArgumentException("Illegal verdi");
+        }
+        for (int i = 0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+            }
+
+        }
+        return a[a.length-1];
+    }
+
 
 }
