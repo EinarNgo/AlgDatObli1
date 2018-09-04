a3b48766d8150b42669ae5372731da86a72ef27d
Einar Ngo <einar_ngo98@hotmail.com>
2018-09-04 07:43:30 +0200

    Oppgave 3, halvferdig

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,8 +1,57 @@
 public class Oblig1 {
 
-    public static void antallUlikeSortert(int[] a) {
+    //Oppgave 2, metode som looper igjennom og sjekker om tabellen stiger, thrower Exception vis
+    //tabellen ikke stiger. Den teller også antall ulike verdier den har i tabellen
+    public static int antallUlikeSortert(int[] a) {
+
+        boolean check = true;
+        int teller=1;
+
+        //Looper gjennom og ser om tabellen stiger
+        for (int i = 0; i <a.length-1; ++i) {
+            if (a[i]<a[i+1]) {
+                teller++;
+            }
+            else if (a[i]==a[i+1]) {
+
+            } else {
+                check = false;
+            }
+        }
+
+        //Kaster Exception hvis tabellen ikke er sortert
+        if (!check) {
+            throw new IllegalStateException("Ikke sortert");
+        }
+        return teller;
 
     }
+
+    //Oppgave 3, metode som looper igjennom som oppgave to, men kan også ha mange like verdier stokket i en array
+    //Sender heller ikke feilkode når tabellen ikke er sortert
+    public static int antallUlikesortert(int[] a) {
+
+        int teller = 0;
+
+        //Kaster nullpointException når tabellen er lik 0
+        if (a.length<1) {
+            throw new NullPointerException("Tabellen er tom");
+        }
+
+        //Looper gjennom tabellen og teller antall ulike verdier
+        for (int i = 0; i < a.length - 1; ++i) {
+
+            for (int j = 0; j<a.length - 1; ++j) {
+                if (a[i] != a[j]) {
+                    teller++;
+                }
+            }
+        }
+
+        return teller;
+    }
+
+
     public static int maks(int[] a) {
         if (a.length < 1) {
             throw new IllegalArgumentException("Illegal verdi");
@@ -17,5 +66,5 @@ public class Oblig1 {
         }
         return a[a.length-1];
     }
-    
+
 }
--- src/Oblig1Test.java
+++ src/Oblig1Test.java
@@ -2,8 +2,12 @@ public class Oblig1Test {
     public static void main(String[] args) {
 
         int[] a = {4,3,5,2,1};
-        System.out.println(Oblig1.maks(a));
+        int[] b = {1,2,3,4,5};
+        int[] c = {1,1,2,2,2};
 
+        //System.out.println(Oblig1.maks(a));
+
+        System.out.println(Oblig1.antallUlikeSortert(c));
     }
     
 }
